eb754c058734262b40373633cf64c0fc955577af
olliestyle <olliestyle1989@gmail.com>
2020-01-31 02:11:40 +0300

    2. Сокобан. Тестовое задание

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -71,6 +71,47 @@ public class Logic {
     public boolean isWin() {
         int[][] table = this.convert();
         boolean result = false;
+        for(int i = 0, j = 0; i < table.length; i++, j++) {
+            if(table[i][j] == 1){
+                if(winHorizontal(table,i) || winVertical(table,j)){
+                    result = true;
+                    break;
+                }
+//                for (int k = 0; k < table[i].length; k++) {
+//                    if(table[i][k] != 1){
+//                        result = false;
+//                    }
+//                }
+//                for (int l = 0; l < table.length; l++){
+//                    if(table[l][j] != 1){
+//                        result = false;
+//                        break;
+//                    }
+//                }
+            }
+        }
+        return result;
+    }
+
+    public boolean winHorizontal(int[][] board, int row){
+        boolean result = true;
+        for(int i = 0; i < board[row].length; i++) {
+            if(board[row][i] != 1){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean winVertical(int[][] board, int column){
+        boolean result = true;
+        for(int i = 0; i < board.length; i++){
+            if(board[i][column] != 1){
+                result = false;
+                break;
+            }
+        }
         return result;
     }
 
